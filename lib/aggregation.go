5e1a27241637d9d66d77293a154fc2c6defa99bf
c <0xe0ffffgmail.com>
2025-07-21 23:14:36 +0800

    sum and count

--- lib/aggregation.go
+++ lib/aggregation.go
@@ -10,9 +10,12 @@ func chooseAggregator(op string) aggregator {
 	var operator aggregator
 	if strings.HasPrefix(op, "first(") {
 		operator = first{name: strings.Replace(strings.Replace(op, "first(", "", -1), ")", "", -1)}
-
 	} else if strings.HasPrefix(op, "first_not_null(") {
 		operator = firstNotNull{name: strings.Replace(strings.Replace(op, "first_not_null(", "", -1), ")", "", -1)}
+	} else if strings.HasPrefix(op, "sum(") {
+		operator = sum{name: strings.Replace(strings.Replace(op, "sum(", "", -1), ")", "", -1)}
+	} else if strings.HasPrefix(op, "count(") {
+		operator = count{name: strings.Replace(strings.Replace(op, "count(", "", -1), ")", "", -1)}
 	}
 	return operator
 }
@@ -39,4 +42,45 @@ func (a firstNotNull) on(collection []map[string]any) any {
 		}
 	}
 	return nil
+}
+
+type sum struct {
+	name string
+}
+
+func (a sum) on(collection []map[string]any) any {
+	var total int64
+	for _, item := range collection {
+		if val, ok := item[a.name]; ok {
+			switch v := val.(type) {
+			case int8:
+				total += int64(v)
+			case int16:
+				total += int64(v)
+			case int32:
+				total += int64(v)
+			case int64:
+				total += v
+			case int:
+				total += int64(v)
+			default:
+				continue
+			}
+		}
+	}
+	return total
+}
+
+type count struct {
+	name string
+}
+
+func (a count) on(collection []map[string]any) any {
+	var total int64
+	for _, item := range collection {
+		if _, ok := item[a.name]; ok {
+			total += 1
+		}
+	}
+	return total
 }
